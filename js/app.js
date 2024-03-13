92c5d25a2d4afe7ed6e74c5d3cf4495585dd1367
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-13 16:13:41 +0100

    Add: BONUS

--- js/app.js
+++ js/app.js
@@ -10,15 +10,53 @@ const playButton = document.getElementById('playbtn')
 //aggiungo event per il click del btn
 playButton.addEventListener('click', function(){
     //genero ciclo for per aggiungere la griglia nell'html
+    
     //seleziono il container nel quale aggiungo la griglia
     const gridContainer = document.querySelector('.grid');
-    
+    //uso .replaceChildren per svuotare gridContainer ogni volta che l'utente clicca sul bottone
+    gridContainer.replaceChildren(); 
+
+    //seleziono il select dal dom
+    const difficultySelect = document.getElementById('difficulty');
+
+    const difficultyOption = difficultySelect.value;
+
+    //numero di celle in una riga
+    let cellRowCount
+    //numero di celle in una colonna
+    let cellColumnCount
+
+    //cambio i valori della griglia (altezza e larghezza in base alla difficoltà)
+    if (difficultyOption === 'difficile') {
+        cellRowCount= 10;
+        cellColumnCount= 10;
+    } else if (difficultyOption === 'normale') {
+        cellRowCount= 9;
+        cellColumnCount= 9;
+    } else {
+        cellRowCount= 7;
+        cellColumnCount= 7;
+    }
+
+    //constante per determinare il numero di celle totali
+    const gridTotalCellCount= cellRowCount * cellColumnCount;
+
     //genero ciclo for
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < gridTotalCellCount; i++) {
         //creo il div della cell con .createElement
         const cellElement = document.createElement('div');
         //aggiungo la classe al div
         cellElement.className = 'cell';
+
+        //cambio la larghezza delle celle in base alla grandezza della griglia
+        if (gridTotalCellCount === 100) {
+            cellElement.classList.add('cell-100');
+        } else if (gridTotalCellCount == 81){
+            cellElement.classList.add('cell-81');
+        } else {
+            cellElement.classList.add('cell-49');
+        }
+        
         //uso .append per mettere la cell nell'apposito container
         gridContainer.append(cellElement)
         
@@ -35,7 +73,6 @@ playButton.addEventListener('click', function(){
         })
     }
 })
-
 
 
 
